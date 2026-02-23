3e96e18adaedf215a779ce95c5d654506433564c
heardchef33 <prachathornth@gmail.com>
2026-02-23 14:28:29 +0700

    feat: added loading function for raw data

---
+++ src/data/loading.py
@@ -0,0 +1,25 @@
+import pandas as pd 
+import time 
+
+def loading(file_path): 
+
+    try: 
+
+        start_time = time.time()
+
+        raw_data = pd.read_csv(file_path)
+
+        elapsed_time = time.time() - start_time
+
+        print(f"Data successfully read in {elapsed_time} seconds!")
+
+        return raw_data
+
+    except FileNotFoundError: 
+
+        print("Error in reading the file. Please try again")
+
+if __name__ == '__main__': 
+
+    loading("/Users/thananpornsethjinda/Desktop/credit-risk-modeling/data/accepted_2007_to_2018Q4.csv")
+
