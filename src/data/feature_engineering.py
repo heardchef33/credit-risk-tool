39d3c9fd083379f12e5716550a91e29f29f1f9c1
heardchef33 <prachathornth@gmail.com>
2026-02-25 16:06:25 +0700

    feat: wrote function to drop correlated columns

--- src/data/feature_engineering.py
+++ src/data/feature_engineering.py
@@ -25,4 +25,31 @@ def dropping_constant_features(X_train: pd.DataFrame, X_val: pd.DataFrame, X_tes
 
     return X_train, X_val, X_test
 
+def correlation_analysis(X_train: pd.DataFrame, X_val: pd.DataFrame, X_test: pd.DataFrame):
+    """
+    Input: data splits 
+    Returns: data splits containing subset of the features from correlation analysis (found in the /notebooks)
+    Purpose: reduce the feature space and multicollinear features 
+    """
+
+    print("Starting correlation analysis ...")
+
+    corr_columns_drop = [
+        'loan_amnt', 
+        'funded_amnt_inv',
+        'open_acc',
+        'pub_rec_bankruptcies',
+        'tot_hi_cred_lim',
+        'avg_cur_bal',
+        'total_bc_limit',
+        'num_actv_bc_tl',
+        'num_rev_tl_bal_gt_0',
+        'num_bc_tl',
+        'num_op_rev_tl',
+        'percent_bc_gt_75',
+        'total_il_high_credit_limit'
+        ]
+    
+    return corr_columns_drop
+
 
