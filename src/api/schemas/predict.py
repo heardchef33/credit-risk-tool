f525cc61d7f0ed606c9b321bb79f2e02cf0d8e53
heardchef33 <prachathornth@gmail.com>
2026-04-20 20:24:27 +0900

    feat: added api and backend code

---
+++ src/api/schemas/predict.py
@@ -0,0 +1,32 @@
+from typing import Any, List, Optional
+
+from pydantic import BaseModel
+from src.processing.validation import LoanDefaultDataInputSchema
+
+class PredictionResults(BaseModel): 
+    errors: Optional[Any]
+    version: str 
+    predictions: Optional[List[float]]
+
+class MultipleLoanDefaultInputs(BaseModel): 
+    inputs: List[LoanDefaultDataInputSchema]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "inputs": [
+                    {
+                        "int_rate" : 2,
+                        "fico_range_high" : 6.0,
+                        "inq_last_6mths" : 1.0,
+                        "open_il_12m" : 1.0,
+                        "acc_open_past_24mths" : 2.0,
+                        "mort_acc" : 2,
+                        "num_tl_op_past_12m" : 3.0,
+                        "percent_bc_gt_75" : 4.0,
+                        "sub_grade" : 'A1',
+                        "term" : '36 months'
+                    }
+                ]
+            }
+        }
